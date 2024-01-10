dd6659bc6d5db65e19b93d4680f33906b61095d3
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-01-11 00:33:07 +0100

    Random color generator js

--- RandomColorGenerator/index.js
+++ RandomColorGenerator/index.js
@@ -1,30 +1,30 @@
 const containerEl = document.querySelector(".container");
 
-for (let i = 0; i < 30; i++) {
-    const colorContainerEl = document.createElement("div");
-    colorContainerEl.classList.add("color-container");
-    containerEl.appendChild(colorContainerEl);
-    
+for (let index = 0; index < 30; index++) {
+  const colorContainerEl = document.createElement("div");
+  colorContainerEl.classList.add("color-container");
+  containerEl.appendChild(colorContainerEl);
 }
 
-const colorContainerEl = document.querySelectorAll(".color-container");
+const colorContainerEls = document.querySelectorAll(".color-container");
 
+generateColors();
 
-const generatorColors = () => {
-    colorContainerEl.forEach((colorContainerEl) => {
-        const newColorCode = randomColor();
-        colorContainerEl.style.backgroundColor = "#" + newColorCode;
-    })
- }
-
-const randomColor = () => {
-    const chars = "0123456789abcdef";
-    const colorCodeLength = 6;
-    let colorCode = "";
-    for (let i = 0; i < colorCodeLength; i++) {
-        const randomNumber = Math.floor(Math.random() * chars.length); 
-        colorCode += chars.substring(randomNumber, randomNumber + 1);
-    }
-    return colorCode;
+function generateColors() {
+  colorContainerEls.forEach((colorContainerEl) => {
+    const newColorCode = randomColor();
+    colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    colorContainerEl.innerText = "#" + newColorCode;
+  });
 }
 
+function randomColor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
+  let colorCode = "";
+  for (let index = 0; index < colorCodeLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNum, randomNum + 1);
+  }
+  return colorCode;
+}
