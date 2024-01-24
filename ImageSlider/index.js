21172e47e76d8a2920edf70a0354d4c657c8312f
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2024-01-24 19:45:17 +0100

    Image Slider js

--- ImageSlider/index.js
+++ ImageSlider/index.js
@@ -1,1 +1,36 @@
+const nextEl = document.querySelector(".next");
+const prevEl = document.querySelector(".prev");
+const imageContainerEl = document.querySelector(".image-container");
+const imgsEl = document.querySelectorAll("img");
 
+let currentImage = 1;
+
+let timeOut;
+
+const updateImg = () => {
+    if(currentImage > imgsEl.length) {
+        currentImage = 1;
+    } else if (currentImage < 1) {
+        currentImage = imgsEl.length;
+    }
+    imageContainerEl.style.transform = `translateX(-${(currentImage - 1) * 500}px)`;
+    timeOut = setTimeout(() => {
+        currentImage++;
+        updateImg()
+    },3000)
+}
+
+updateImg();
+
+
+nextEl.addEventListener("click", () => {
+    currentImage++;
+    clearTimeout(timeOut);
+    updateImg()
+});
+
+prevEl.addEventListener("click", () => {
+    currentImage--;
+    clearTimeout(timeOut);
+    updateImg()
+})
